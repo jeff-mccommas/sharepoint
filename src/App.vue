dbf171e8fdce34777728d538d4e80006566e5d01
jeff-mccommas <jeffmccommas@gmail.com>
2019-01-06 14:57:56 -0500

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,50 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <app-header></app-header>
+    <transition name="slide" mode="out-in">
+      <router-view/>
+    </transition>
   </div>
 </template>
+<script>
+import Header from "./components/Header.vue";
+import Home from "./views/Home.vue";
+export default {
+  components: {
+    appHeader: Header,
+    Home: Home
+  }
+};
+</script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
+<style>
+body {
   padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+}
+.slide-enter-active {
+  animation: slide-in 200ms ease-out forwards;
+}
+.slide-leave-active {
+  animation: slide-out 200ms ease-out forwards;
+}
+@keyframes slide-in {
+  from {
+    transform: translateY(-30px);
+    opacity: 0;
+  }
+  to {
+    transform: translateY(0);
+    opacity: 1;
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateY(0);
+    opacity: 1;
+  }
+  to {
+    transform: translateY(-30px);
+    opacity: 0;
   }
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+   <b-navbar toggleable="md" type="dark" variant="info">
+
+  <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
+
+  <b-navbar-brand <router-link tag="a" to="/" activeClass="active">Share Point App </router-link></b-navbar-brand>
+
+  <b-collapse is-nav id="nav_collapse">
+
+    <b-navbar-nav>
+      <b-nav-item href="#">Link</b-nav-item>
+      <b-nav-item href="#" disabled>Disabled</b-nav-item>
+    </b-navbar-nav>
+
+    <!-- Right aligned nav items -->
+    <b-navbar-nav class="ml-auto">
+
+
+    </b-navbar-nav>
+
+  </b-collapse>
+</b-navbar>
+
+  </div>
+</template>
+
+<script>
+export default {
+  appHeader: 'Header',
+  data () {
+    return {}
+  },
+  computed: {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+a.router-link-exact-active.active {
+    color: #fff;
+    font-weight: bold;
+    font-style: normal;
+    text-decoration: none;
+    background-color: transparent;
+    /* font-family: Arial, Helvetica, sans-serif */
+}
+</style>
